e39327f92d720ba0528543095c270f6c6d10c252
Liam Roche <roche.liamp@gmail.com>
2022-03-29 00:32:52 -0400

    added image upload capability

--- src/MainComponent/SecretFormComponent/SecretFormComponent.jsx
+++ src/MainComponent/SecretFormComponent/SecretFormComponent.jsx
@@ -1,26 +1,38 @@
 import { useState } from 'react'
 import './SecretFormComponent.css'
 
-
 function SecretFormComponent(props){;
     const [newSecret, setNewSecret] = useState({})
-
     //function to handle change of text in input fields uses e-target so can be assigned to any input field
     const handleInputChange=(e)=>{
         console.log(newSecret)
         //sets new secret variable 
         setNewSecret({
-           secret: e.target.value
+           secret: e.target.value,
+           url: url
         })
+        console.log(newSecret.url)
     }
-    
     const submitNewSecret = (e)=>{
             e.preventDefault();
             props.createNewSecret(newSecret)
             props.revealSecrets()
         }
             //ENDLESS VALIDATION IF CHAINS GO HERE
-    
+    const [image, setImage] = useState("");
+    const [url, setUrl] = useState("")
+    const uploadImage = async ()=>{
+        const data = new FormData();
+        data.append('file', image);
+        data.append('upload_preset', 'ym3qlxdj')
+        // data.append('cloud_name', process.env.IMG_CLOUD_NAME)
+        const imgUplaodResponse = await fetch(`https://api.cloudinary.com/v1_1/${process.env.REACT_APP_IMG_CLOUD}/image/upload`, {
+            method: 'POST',
+            body: data
+        })
+        const parsedResponse = await imgUplaodResponse.json()
+        setUrl(parsedResponse.url)
+    }
     return(
         <div className="SecretFormComponent" id="secret-form">
             <h1>Please enter a secret:</h1>
@@ -30,6 +42,17 @@ function SecretFormComponent(props){;
                     <button className="btn btn-outline-secondary" type="submit">Submit Secret</button>
                 </div>
             </form>
+            <input type="file" name="file" onChange={(e)=>{setImage(e.target.files[0])}}></input>
+            <div>
+                <p>Image Name: {image.name}</p>
+                <p>File Type: {image.type}</p>
+                <p>Size: {image.size}</p>
+                <button onClick={uploadImage}>Add Artwork</button>
+                </div>
+                <div>
+                    <p>Here is the image:</p>
+                    <img src={url}/>
+                </div>
         </div>
     )  
 }
