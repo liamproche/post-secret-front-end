fc1d292971ebf4dcf45234c1a21845e379ed3015
Liam Roche <roche.liamp@gmail.com>
2022-03-31 15:57:54 -0400

    more styling touchups

--- src/MainComponent/SecretsComponent/SecretsComponent.jsx
+++ src/MainComponent/SecretsComponent/SecretsComponent.jsx
@@ -4,11 +4,18 @@ import IndividualSecretComponent from './IndividualSecretComponent/IndividualSec
 
 
 function SecretsComponent(props){
+    function reloadSite(){
+        window.location.reload()
+    }
     return(
         <section className="SecretsComponent" id="secret-component">
+            <h2>Secrets created by others:</h2>
             {props.secrets.map((secret)=>{
                 return <IndividualSecretComponent key={secret._id} secret={secret} deleteSecret={props.deleteSecret} editSecret={props.editSecret}></IndividualSecretComponent>
             })}
+            <div id="home-link-container">
+                <h2 onClick={reloadSite}>Submit a new secret</h2>
+            </div>
         </section>
     )  
 }
